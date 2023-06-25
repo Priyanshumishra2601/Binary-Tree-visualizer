64d750473ff8e384e17499f165016fe447bc1f57
Priyanshu mishra <116107882+Priyanshumishra2601@users.noreply.github.com>
2023-06-25 19:47:52 +0530

    Add files via upload

---
+++ RequesterLogin.php
@@ -0,0 +1,113 @@
+<?php 
+ include('../dbConnection.php');
+ session_start();
+ if(!isset($_SESSION['is_login'])){
+  if(isset($_REQUEST['rEmail'])){
+   $rEmail = mysqli_real_escape_string($conn, trim($_REQUEST['rEmail']));
+   $rPassword = mysqli_real_escape_string($conn, trim($_REQUEST['rPassword']));
+   $sql = "SELECT r_email, r_password FROM requesterlogin_tb WHERE r_email = '".$rEmail."' AND r_password = '".$rPassword."' limit 1";
+   $result = $conn->query($sql);
+   if($result->num_rows == 1){
+    $_SESSION['is_login'] = true;
+    $_SESSION['rEmail'] = $rEmail;
+    echo "<script> location.href='RequesterProfile.php';</script>";
+    exit;
+   } else {
+    $msg = '<div class="alert alert-warning mt-2">Enter Valid Email and Password</div>';
+   }
+  }
+ } else {
+  echo "<script> location.href='RequesterProfile.php';</script>";
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+ <!-- Font Awesome CSS -->
+ <link rel="stylesheet" href="../css/all.min.css">
+
+ <style>
+  .custom-margin {
+   margin-top: 8vh;
+  }
+ </style>
+
+ <title>Login</title>
+</head>
+<body>
+ <div class="mb-3 mt-5 text-center" style="font-size: 30px;">
+  <i class="fas fa-stethoscope"></i>
+  <span>Online Service Management System</span>
+ </div>
+ <p class="text-center" style="font-size:20px;"><i class="fas fa-user-secret text-danger"></i>Requester Area (Demo)</p>
+ <div class="container-fluid">
+  <div class="row justify-content-center custom-margin">
+   <div class="col-sm-6 col-md-4">
+    <form action="" class="shadow-lg p-4" method="POST">
+     <div class="form-group">
+      <i class="fas fa-user"></i><label for="email" class="font-weight-bold pl-2">Email</label><input type="email" class="form-control" placeholder="Email" name="rEmail">
+      <small class="form-text">We'll never share your email with anyone else.</small>
+     </div>
+     <div class="form-group">
+      <i class="fas fa-key"></i><label for="pass" class="font-weight-bold pl-2">Password</label><input type="password" class="form-control" placeholder="Password" name="rPassword">
+     </div>
+     <button type="submit" class="btn btn-outline-danger mt-3 font-weight-bold btn-block shadow-sm">Login</button>
+     <?php if(isset($msg)) {echo $msg;} ?>
+    </form>
+    <div class="text-center"><a href="../index.php" class="btn btn-info mt-3 font-weight-bold shadow-sm">Back to Home</a></div>
+   </div>
+  </div>
+ </div>
+
+ <!-- JavaScript Files -->
+ <script src="../js/jquery.min.js"></script>
+ <script src="../js/popper.min.js"></script>
+ <script src="../js/bootstrap.min.js"></script>
+ <script src="../js/all.min.js"></script>
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+</html>
